174d6dc31023634415a50d960e0ab380c92797e6
manoharmaripe <manoharmaripe.19@gmail.com>
2024-12-17 12:13:44 +0530

    gallery

--- src/Component/Gallery/Gallery.jsx
+++ src/Component/Gallery/Gallery.jsx
@@ -8,11 +8,11 @@ const Gallery = () => {
     <div>
       <MainLayout>
         <div className="flex items-center justify-center md:mt-6 mb-6 sm:mt-10 px-6">
-          <div className="relative w-full sm:w-[90%] h-28 lg:h-32 bg-slate-400 shadow-lg rounded-lg px-4 py-2">
+          <div className="relative w-full sm:w-[90%] h-20 sm:h-24 lg:h-32 bg-slate-400 shadow-lg rounded-lg px-4 py-2">
             <div className="flex items-center justify-center text-xl md:text-2xl lg:text-3xl w-full h-full">
               {eventsData[0].events[0].title}
             </div>
-            <div className="absolute text-sm md:text-base lg:text-xl right-3 md:right-6 bottom-2 md:bottom-4">
+            <div className="absolute text-xs sm:text-sm md:text-base lg:text-xl right-3 md:right-6 bottom-2 md:bottom-4">
               -{eventsData[0].events[0].date}
             </div>
           </div>
